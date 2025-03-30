c229a191f3e3747dc5f18e087ba9f6f23ce095ca
cazzysoci <carielglobio@gmail.com>
2025-03-31 01:05:09 +0800

    Update webflood.py

--- webflood.py
+++ webflood.py
@@ -51,7 +51,7 @@ def attack():
         else:
             break
 
-def id_generator(size=12, chars=string.ascii_uppercase + string.digits):
+def id_generator(size=12, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 if __name__ == "__main__":
